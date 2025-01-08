6733ea8757c31fd0e79cdc81c5eb611a964500be
Zual Rana <freelancerzual@gmail.com>
2025-01-08 13:42:48 +0600

    display features and bottom

--- js/app.js
+++ js/app.js
@@ -5,6 +5,50 @@ const loadAllData = async () => {
   displayData(data.data.tools);
 };
 
-const displayData = (tools) => {};
+const displayData = (tools) => {
+  const cardContainer = document.getElementById("card-container");
+  tools.forEach((tool) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "bg-base-100", "w-96", "shadow-xl");
+    cardDiv.innerHTML = `
+        <figure>
+            <img src="${tool.image}"
+            alt="Shoes" />
+        </figure>
+        <div class="px-5 py-2">
+    <h2 class="card-title">Features</h2>
+  </div>
+    `;
+    tool?.features.forEach((feature, index) => {
+      const div = document.createElement("div");
+      div.classList.add("px-5", "py-2");
+      const p = document.createElement("p");
+      p.innerText = `${index + 1}. ${feature}`;
+      div.appendChild(p);
+      cardDiv.appendChild(div);
+    });
+    // card bottom
+    const cardBottomDiv = document.createElement("div");
+    cardBottomDiv.classList.add(
+      "flex",
+      "justify-between",
+      "items-center",
+      "px-5",
+      "py-5"
+    );
+    cardBottomDiv.innerHTML = `
+        <div> 
+        <h4 class="text-xl font-bold"> ${tool.name}</h4>
+        <p> ${tool.published_in} </p>
+        </div>
+        <div class="text-red-500"> <a class="cursor-pointer"> &#8594; </a> </div>
+    `;
+    // append card bottom div into card div
+    cardDiv.appendChild(cardBottomDiv);
+
+    // append card into card container
+    cardContainer.appendChild(cardDiv);
+  });
+};
 
 loadAllData();
